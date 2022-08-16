f2995dd98b8bfa607a6707ef247ef5c6dc0bda6d
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-08-16 18:23:12 -0500

    Change icon

--- src/app/alumnos/modify/modify.component.ts
+++ src/app/alumnos/modify/modify.component.ts
@@ -10,9 +10,10 @@ import { ServiceAlumnoService } from 'src/app/services/service-alumno.service';
 })
 export class ModifyComponent implements OnInit {
   public formDisabled = true;
- 
+  loading: boolean = false; // Flag variable
+  file: File = null; // Variable to store file
   alumnos = new FormGroup({
-    id:new FormControl({value:'', disabled: this.formDisabled}),
+    id: new FormControl({ value: '', disabled: this.formDisabled }),
     name: new FormControl(''),
     firstName: new FormControl(''),
     lastName: new FormControl(''),
@@ -24,16 +25,46 @@ export class ModifyComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
-    
+
   }
-  updateAlumno(){
-    this.alumnos.value.id=this.data.id;
+  updateAlumno() {
+    this.alumnos.value.id = this.data.id;
     this.dialogo.close();
-    this.serviceAlumno.UpdateAlumno(this.alumnos.value).subscribe((res:any) => {
+    this.serviceAlumno.UpdateAlumno(this.alumnos.value).subscribe((res: any) => {
       console.log("res");
-      
+
     });
     this.serviceAlumno.sendClickEvent(this.alumnos.value);
+  }
+  onChange(event: Event) {
+
+    this.file = (event.target as HTMLInputElement).files[0];
+
+  }
+  onUpload() {
+    if (this.file != null) {
+      this.serviceAlumno.SaveImage(this.file).subscribe(
+        (res) => {
+          console.log("send--------" + JSON.stringify(res));
+          this.save(res.url);
+
+        }
+      );
+    }
+    else{
+      this.updateAlumno();
+    }
+  }
+  save(image_url: string) {
+    this.dialogo.close();
+
+    this.serviceAlumno.CreateAlumno(this.alumnos.value, image_url).subscribe((res) => {
+      console.log('Issue added!' + JSON.stringify(this.alumnos.value.firstName));
+
+    });
+    this.serviceAlumno.sendClickEvent(this.alumnos.value);
+
+
   }
 
 }
